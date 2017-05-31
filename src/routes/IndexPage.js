2cce0d90e5955e26dbc0afee0e91db2d66ba7907
GuoChen-WHU <496474070@qq.com>
2017-05-31 22:23:48 +0800

    feat: layout

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'dva';
 import { routerRedux } from 'dva/router'; 
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
+const SubMenu = Menu.SubMenu;
 import styles from './IndexPage.css';
 
 import Stars from '../components/Stars/Stars';
@@ -24,10 +27,42 @@ class IndexPage extends Component {
 
   render() {
     return (
-      <div className={styles.normal}>
-        <h1 className={styles.title}>Github Stars</h1>
-        <Stars />
-      </div>
+      <Layout>
+        <Sider>
+          <h1 className={styles.logo}>Github Stars</h1>
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <span>
+                <Icon type="star" />
+                <span className="nav-text">Stars</span>
+              </span>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={<span><Icon type="switcher" /><span className="nav-text">Archive</span></span>}
+            >
+              <Menu.Item key="2">Archive 1</Menu.Item>
+              <Menu.Item key="3">Archive 2</Menu.Item>
+              <Menu.Item key="4">Archive 3</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff' }} />
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '12px 0' }}>
+              <Breadcrumb.Item>Stars</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{ padding: 24, paddingBottom: 60, background: '#fff', minHeight: 360 }}>
+              <Stars />
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Github Stars ©2016 Created by Guo Chen
+          </Footer>
+        </Layout>
+      </Layout>
     );
   }
 }
